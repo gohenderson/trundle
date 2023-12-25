2e8afabfe92df9f73cffbc19d1504596eef7afdf
Greg Henderson <gohenderson@gmail.com>
2023-12-25 07:20:43 -0500

    I hashor.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use sha256::digest;
+use std::io;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("Enter a value to hash: ");
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let input = String::from(line.unwrap());
+        let val = digest(input);
+        println!("Hashed Value: {}", val);
+    }
+
 }
