f865c8071cf032e33d6a3f9c6077afea721fd2f1
Greg Henderson <gohenderson@gmail.com>
2023-12-28 06:50:59 -0500

    gRPC. I have ideas, man. Ideas.

---
+++ src/server.rs
@@ -0,0 +1,40 @@
+use tonic::{transport::Server, Request, Response, Status};
+
+use trundle::greeter_server::{Greeter, GreeterServer};
+use trundle::{HelloReply, HelloRequest};
+
+pub mod trundle {
+    tonic::include_proto!("trundle");
+}
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>, // Accept request of type HelloRequest
+    ) -> Result<Response<HelloReply>, Status> { // Return an instance of type HelloReply
+        println!("Got a request: {:?}", request);
+
+        let reply = trundle::HelloReply {
+            message: format!("Hello {}!", request.into_inner().name).into(), // We must use .into_inner() as the fields of gRPC requests and responses are private
+        };
+
+        return Ok(Response::new(reply)); // Send back our formatted greeting
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let greeter = MyGreeter::default();
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    return Ok(());
+}
